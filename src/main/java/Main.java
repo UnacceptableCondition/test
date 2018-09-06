f65d599ea9d64b38f520956a5727c53042ade7c8
Unacceptable Condition <atjers.inrau@gmail.com>
2018-09-06 11:12:07 +0300

    for transfer

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.reactive.function.server.HandlerFunction;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import reactor.ipc.netty.http.server.HttpServer;
+
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromObject;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        HandlerFunction hello = request -> ok().body(fromObject("Hello1"));
+
+        RouterFunction router = route(GET("/"), hello).andRoute(
+                GET("/json"), req ->
+                        ok()
+                                .contentType(APPLICATION_JSON)
+                                .body(fromObject(new Hello("World"))));
+
+        HttpHandler httpHandler = RouterFunctions.toHttpHandler(router);
+
+        HttpServer
+                .create("localhost", 8080)
+                .newHandler(new ReactorHttpHandlerAdapter(httpHandler))
+                .block();
+
+        Thread.currentThread().join();
+    }
+}
